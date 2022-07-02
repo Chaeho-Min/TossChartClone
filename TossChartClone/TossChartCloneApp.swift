ce5d69191c48bd4435010aec0edd44cf68baef32
Chaeho-Min <james118@naver.com>
2022-07-02 19:45:35 +0900

    Initial Commit

---
+++ TossChartClone/TossChartCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TossChartCloneApp.swift
+//  TossChartClone
+//
+//  Created by 민채호 on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct TossChartCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
